9662ffd46be80e6bd83d49a33059976bdde4163a
j_szuminski <jakubszuminskixrp@gmail.com>
2022-09-23 12:51:22 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//COPYRIGHT CURRENT YEAR
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+//SMOOTH SCROLLING
+const allLinks = document.querySelectorAll(".inpage:link");
+console.log(allLinks);
+
+allLinks.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const href = link.getAttribute("href");
+
+        if(href==="#") 
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+        
+        if(href !== "#" && href.startsWith("#")) {
+            const sectionEl = document.querySelector(href);
+            sectionEl.scrollIntoView({behavior: "smooth"});
+        }
+    });
+});
---
+++ readme.md
@@ -0,0 +1,1 @@
+# rewer-main-landingpage 
